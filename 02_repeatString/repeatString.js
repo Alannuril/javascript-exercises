741bcb834685f27c3b161e1fa00937f7f0c90cc8
Alan Nuril <alannuril013@gmail.com>
2025-02-03 17:27:24 +0700

    work on question 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
+let repeat = 3;
+const str = "hay"
 
-};
+const repeatString = function (string, num) {
+    if (num === 0) {
+        return "Error";
+    }
+    let result = '';
+    for (let i = 0; i < num; i++) {
+        result += string
+    }
+    return result;
+}
 
-// Do not edit below this line
 module.exports = repeatString;
